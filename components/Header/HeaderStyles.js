ddcaf0e33b856b2eee439e6e22de28f525f2a748
raynertjx <rayner742@gmail.com>
2022-07-18 23:03:49 +0800

    Made header position fixed

--- components/Header/HeaderStyles.js
+++ components/Header/HeaderStyles.js
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 
 export const Container = styled.header`
+    width: 100%;
+    position: fixed;
     display: flex;
     justify-content: space-between;
     align-items: center;
