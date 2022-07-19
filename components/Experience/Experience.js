801542d77e0e402bcc7a7b08c96d07aaa786d51b
raynertjx <rayner742@gmail.com>
2022-07-19 19:35:28 +0800

    Added key prop

--- components/Experience/Experience.js
+++ components/Experience/Experience.js
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Heading } from "../UI/Heading";
 import { Section } from "../UI/Section";
+import { TabList, ContentDiv } from "./ExperienceStyles";
+import { Jobs } from "../../lib/constants";
 
 const Experience = () => {
+    const [selectedBtnId, setSelectedBtnId] = useState("m1");
+    const [selectedJob, setSelectedJob] = useState();
+    useEffect(() => {
+        const job = Jobs.find((item) => item.id === selectedBtnId);
+        setSelectedJob(job);
+    }, [selectedBtnId]);
+
+    const buttonClickHandler = (e) => {
+        setSelectedBtnId(e.target.id);
+    };
+
     return (
         <Section>
             <Heading>Experience</Heading>
+            <div>
+                <TabList>
+                    {Jobs.map((item) => (
+                        <button onClick={buttonClickHandler} key={item.id} id={item.id}>
+                            {item.button_title}
+                        </button>
+                    ))}
+                </TabList>
+                <ContentDiv>
+                        {/* <h3>{selectedJob.job_title}</h3>
+                        <p>{selectedJob.job_date}</p> */}
+                </ContentDiv>
+            </div>
         </Section>
     );
 };
--- components/Experience/ExperienceStyles.js
+++ components/Experience/ExperienceStyles.js
@@ -1,1 +1,10 @@
 import styled from "styled-components";
+
+export const TabList = styled.div`
+    display: flex;
+    flex-direction: column;
+`
+
+export const ContentDiv = styled.div`
+
+`
