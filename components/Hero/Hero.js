201a156ee99b2d1c01463df57da8a3c832739082
raynertjx <rayner742@gmail.com>
2022-07-19 15:45:40 +0800

    Changed styles and description

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -1,12 +1,16 @@
-import { HeroDesc } from "../../lib/constants";
+import { HeroDesc1, HeroDesc2 } from "../../lib/constants";
 import { Section, IntroHeader, NameHeader, Intro } from "./HeroStyles";
 
 const Hero = () => {
     return (
         <Section>
             <IntroHeader>Hi, my name is</IntroHeader>
-            <NameHeader>Rayner Toh.</NameHeader>
-            <Intro>{HeroDesc}</Intro>
+            <NameHeader>
+                Rayner Toh<span>.</span>
+            </NameHeader>
+            <Intro>
+                <p>{HeroDesc1}</p>
+            </Intro>
         </Section>
     );
 };
--- components/Hero/HeroStyles.js
+++ components/Hero/HeroStyles.js
@@ -11,16 +11,22 @@ export const Section = styled.section`
 export const IntroHeader = styled.h3`
     color: ${({ theme }) => theme.colors.accentOrange};
     font-family: ${({ theme }) => theme.fonts.mono};
-    font-size: 1.2rem;
+    font-size: 1.4rem;
     margin: 0 4px;
 `;
 
 export const NameHeader = styled.h2`
-    font-size: 7rem;
+    font-size: 6.5rem;
     padding-bottom: 1rem;
+    span {
+        font-size: 4rem;
+    }
 `;
 
-export const Intro = styled.p`
-    font-size: 1.5rem;
-    margin: 0 4px;
+export const Intro = styled.div`
+    color: #b2b2b2;
+    p {
+        font-size: 1.75rem;
+        margin: 0 4px;
+    }
 `;
