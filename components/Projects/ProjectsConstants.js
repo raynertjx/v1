1c2cc5ac53ac8871495108e354a3aa31870b5bb3
raynertjx <rayner742@gmail.com>
2022-07-26 21:31:21 +0800

    Added first project constants

---
+++ components/Projects/ProjectsConstants.js
@@ -0,0 +1,13 @@
+import Image1 from "../../lib/img/help-neighbour-mockup.jpg";
+
+export const projects = [
+    {
+        id: 0,
+        img: Image1,
+        title: "Help A Neighbour Out",
+        desc: "A Telegram bot that allows you to ask or receive help from neighbours in your vicinity using your postal code! Submission for LifeHack 2022.",
+        skills: ["Python", "Firebase", "Heroku", "Telegram"],
+        github_link: "https://github.com/bkjwjason/Hackathon-Telegram-Bot",
+        deployment_link: "https://devpost.com/software/gedong-cs-boys",
+    },
+];
