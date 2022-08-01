b30977eee14c2beb59150904f1ea309850641566
raynertjx <rayner742@gmail.com>
2022-08-01 18:17:30 +0800

    Added responsive style for hero

--- components/Hero/HeroStyles.js
+++ components/Hero/HeroStyles.js
@@ -8,23 +8,22 @@ export const HeroSection = styled(Section)`
 export const IntroHeader = styled.h3`
     color: ${({ theme }) => theme.colors.accentOrange};
     font-family: ${({ theme }) => theme.fonts.mono};
-    font-size: 1.4rem;
-    margin: 0 6px;
+    font-size: clamp(1rem, 3vw, 1.4rem);
+    margin-left: 2px;
 `;
 
 export const NameHeader = styled.h2`
     color: ${({ theme }) => theme.colors.white};
-    font-size: 6.5rem;
-    padding-bottom: 1rem;
+    font-size: clamp(3.5rem, 12vw, 6.5rem);
     span {
-        font-size: 4rem;
+        font-size: clamp(2rem, 8vw, 4rem);
     }
 `;
 
 export const Intro = styled.div`
     color: #b2b2b2;
     p {
-        font-size: 1.75rem;
+        font-size: clamp(1rem, 3.5vw, 1.75rem);
         margin: 0 6px;
     }
 `;
