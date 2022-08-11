5a5296a483322d7e574b417c2df6161981aec4ad
raynertjx <rayner742@gmail.com>
2022-08-11 23:58:37 +0800

    Created new AboutConstants.js file

---
+++ components/Experience/ExperienceConstants.js
@@ -0,0 +1,20 @@
+export const Jobs = [
+    {
+        id: 0,
+        place: "Singapore Armed Forces",
+        role: "Operations Specialist",
+        date: "Sep 2020 - Nov 2021",
+        content: [
+            "Maintained database to help track servicemen soldier fundamentals in Excel",
+            "Assisted superiors in administrative tasks such as schedule planning and map drawing",
+            "Helped procure logistics for company events",
+        ],
+    },
+    {
+        id: 1,
+        place: "404",
+        role: "error",
+        date: "soon...",
+        content: ["work in progress..."],
+    },
+];
