d09f1b0ae78dab415ee307445cc1d7007430e5ac
raynertjx <rayner742@gmail.com>
2022-08-11 18:24:29 +0800

    Removed position relative

--- components/UI/Heading.js
+++ components/UI/Heading.js
@@ -3,7 +3,6 @@ import styled from "styled-components";
 export const Heading = styled.h1`
     font-size: clamp(2rem, 5vw, 2.25rem);
     font-weight: 600;
-    position: relative;
     padding: 1.5em 0;
     font-family: ${({ theme }) => theme.fonts.mono};
     color: ${({ theme }) => theme.colors.accentOrange};
