453b74fe23f86a70845e28a110f91f21238e5475
raynertjx <rayner742@gmail.com>
2022-08-14 19:47:29 +0800

    Added socials to footer

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,7 +1,30 @@
-import { FooterDiv } from "./FooterStyles";
+import { FooterContainer, FooterDiv, SocialsDiv } from "./FooterStyles";
+import { FiGithub, FiTwitter, FiLinkedin } from "react-icons/fi";
 
 const Footer = () => {
-    return <FooterDiv><a href="https://github.com/raynertjx/portfolio">Designed & Built by Rayner Toh</a></FooterDiv>;
+    return (
+        <FooterContainer>
+            <SocialsDiv>
+                <a href="https://github.com/raynertjx" target="_blank">
+                    <FiGithub />
+                </a>
+                <a href="https://twitter.com/raynertjx" target="_blank">
+                    <FiTwitter />
+                </a>
+                <a
+                    href="https://www.linkedin.com/in/rayner-toh-90a3b11b4/"
+                    target="_blank"
+                >
+                    <FiLinkedin />
+                </a>
+            </SocialsDiv>
+            <FooterDiv>
+                <a href="https://github.com/raynertjx/portfolio" target="_blank">
+                    Designed & Built by Rayner Toh
+                </a>
+            </FooterDiv>
+        </FooterContainer>
+    );
 };
 
 export default Footer;
--- components/Footer/FooterStyles.js
+++ components/Footer/FooterStyles.js
@@ -1,14 +1,33 @@
 import styled from "styled-components";
 
-export const FooterDiv = styled.footer`
+export const FooterContainer = styled.footer`
     display: flex;
+    flex-direction: column;
     align-items: center;
     justify-content: center;
-    padding-bottom: 1.5rem;
+    gap: 1em;
+`;
+
+export const FooterDiv = styled.div`
     font-size: 0.8rem;
+    padding-bottom: 1em;
     a {
         &:hover {
-            transition: color 0.3s ease-in-out; 
+            transition: color 0.3s ease-in-out;
+            color: ${(props) => props.theme.colors.accentOrange};
+        }
+    }
+`;
+
+export const SocialsDiv = styled.div`
+    display: flex;
+    gap: 2em;
+    svg {
+        width: 20px;
+        height: 20px;
+        cursor: pointer;
+        transition: 0.3s ease-out;
+        :hover {
             color: ${(props) => props.theme.colors.accentOrange};
         }
     }
