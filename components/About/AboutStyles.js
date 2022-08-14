8828dd8af3dc262bcd589763cf5e1baabf786606
raynertjx <rayner742@gmail.com>
2022-08-14 17:48:26 +0800

    Adjusted max-width

--- components/About/AboutStyles.js
+++ components/About/AboutStyles.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const AboutSection = styled.section`
-    max-width: 1000px;
+    max-width: 900px;
     color: ${({ theme }) => theme.colors.white};
 `;
 
@@ -14,7 +14,7 @@ export const Info = styled.div`
 `;
 
 export const List = styled.ul`
-    list-style-type: none;
+    list-style-type: none;  
     display: grid;
     grid-template-columns: repeat(2, minmax(100px, 200px));
     gap: 0.5rem 1rem;
